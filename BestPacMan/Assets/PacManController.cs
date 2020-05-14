076ae6978621cfa15f3eb566962c0baf25ed0814
NDUDLEY27 <nathaniel.dudley@hgs.hiddengeniusproject.org>
2020-05-14 14:26:57 -0700

    Created Movement

--- BestPacMan/Assets/PacManController.cs
+++ BestPacMan/Assets/PacManController.cs
@@ -8,9 +8,18 @@ public class PacManController : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    Vector2 movement;
+
     // Update is called once per frame
     void Update()
     {
-        
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
